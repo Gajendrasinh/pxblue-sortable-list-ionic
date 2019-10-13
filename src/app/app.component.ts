ff4ac8af6ab8fba2231d6296d801a493cf5828d4
Gajendrasinh Zala <gajendrasinh.zala93@gmail.com>
2019-10-14 07:40:44 +0800

    defect fixed

---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component} from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { VERSION, MatMenuTrigger } from '@angular/material';
+import { moveItemInArray } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  version = VERSION;
+
+  data = [{
+    'name': 'George Washington',
+    'details': '1789'
+  },
+  {
+    'name': 'John Adams',
+    'details': '1796'
+  },
+  { 'name': 'Thomas Jefferson', 'details': '1800' },
+  { 'name': 'James Madison', 'details': '1808' },
+  { 'name': 'James Monroe', 'details': '1812' }
+  ];
+  item: any = "";
+  draggable: boolean = false;
+
+  displayedColumns: string[] = ['name', 'details'];
+  dataSource: any;
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  ngOnInit(): void {
+   
+  }
+
+  onDrop(event) {
+    moveItemInArray(this.data, event.previousIndex, event.currentIndex);
+  }
+  edit() {
+    this.draggable = !this.draggable;
+  }
+
+ 
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { NgModule,  } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import {
+  MatToolbarModule, MatListModule, MatIconModule
+} from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    IonicModule.forRoot(), 
+    BrowserAnimationsModule, 
+    HttpClientModule,
+    FlexLayoutModule,
+    MatToolbarModule, 
+    MatListModule,
+    MatIconModule,
+    DragDropModule 
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
